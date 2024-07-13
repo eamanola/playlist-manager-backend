c062500f1d1d55d00d104df1f54d9a37ae6dfbba
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-07-13 11:31:56 +0300

    add env

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-const MEDIA_LIBS = ['/home/eamanola/Videos', '/home/eamanola/Downloads'];
-const PLAYER = 'open';
-const THUMB_DIR = '/tmp/media-library/thumbnails';
-const { NODE_ENV } = process.env;
+const {
+  MEDIA_LIBS = '/home/eamanola/Videos',
+  NODE_ENV,
+  PLAYER = 'open',
+  THUMB_DIR = '/tmp/media-library/thumbnails',
+} = process.env;
 
 module.exports = {
-  MEDIA_LIBS,
+  MEDIA_LIBS: MEDIA_LIBS ? MEDIA_LIBS.split(':') : [],
   NODE_ENV,
   PLAYER,
   THUMB_DIR,
