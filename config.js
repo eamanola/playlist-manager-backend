4c11ecfe130e4995d5b051d167b46415fec25bcd
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-07-25 12:48:34 +0300

    CACHE_DIR

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 const {
+  CACHE_DIR = '/tmp/media-library',
   MEDIA_LIBS = '/home/eamanola/Videos',
   NODE_ENV,
   PLAYER = 'open',
-  THUMB_DIR = '/tmp/media-library/thumbnails',
 } = process.env;
 
 module.exports = {
+  CACHE_DIR,
   MEDIA_LIBS: MEDIA_LIBS ? MEDIA_LIBS.split(':') : [],
   NODE_ENV,
   PLAYER,
-  THUMB_DIR,
+  THUMB_DIR: `${CACHE_DIR}/thumbnails`,
 };
