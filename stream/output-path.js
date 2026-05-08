d0ff67f7730a0478e48ecfc258a8e53da5ea17e7
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-08 17:38:19 +0300

    limit cache name length

--- stream/output-path.js
+++ stream/output-path.js
@@ -1,10 +1,22 @@
 const { mkdir } = require('node:fs/promises');
 const { sep } = require('node:path');
 
+const { utils } = require('automata-utils');
+
 const { CACHE_DIR } = require('../config');
 
+const { logger } = utils;
+
 const outputDir = async (type, path) => {
-  const sepReplaced = path.split(sep).filter((segment) => !!segment).join('-');
+  let sepReplaced = path.split(sep).filter((segment) => !!segment).join('-');
+
+  // Error: ENAMETOOLONG
+  const MAX_LENTH = 100;
+  if (sepReplaced.length > MAX_LENTH) {
+    logger.warn(`cutting ${sepReplaced} to ${MAX_LENTH}`);
+
+    sepReplaced = sepReplaced.substring(0, MAX_LENTH);
+  }
 
   const tempDir = `${CACHE_DIR}/media/${sepReplaced}/${type}`;
   await mkdir(tempDir, { recursive: true });
