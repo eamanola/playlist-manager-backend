28364a8d658175b1d28075110feb05136de155e2
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-09 20:41:53 +0300

    hash part of output names

--- stream/output-path.js
+++ stream/output-path.js
@@ -1,24 +1,17 @@
 const { mkdir } = require('node:fs/promises');
-const { sep } = require('node:path');
-
-const { utils } = require('automata-utils');
+const { sep, join } = require('node:path');
+const { createHash } = require('node:crypto');
 
 const { CACHE_DIR } = require('../config');
 
-const { logger } = utils;
-
 const outputDir = async (type, path) => {
-  let sepReplaced = path.split(sep).filter((segment) => !!segment).join('-');
+  const sepReplaced = path.split(sep).filter((segment) => !!segment).join('-');
 
   // Error: ENAMETOOLONG
-  const MAX_LENTH = 100;
-  if (sepReplaced.length > MAX_LENTH) {
-    logger.warn(`cutting ${sepReplaced} to ${MAX_LENTH}`);
+  // names might be too long
+  const hashed = createHash('md5').update(sepReplaced).digest('hex');
 
-    sepReplaced = sepReplaced.substring(0, MAX_LENTH);
-  }
-
-  const tempDir = `${CACHE_DIR}/media/${sepReplaced}/${type}`;
+  const tempDir = join(CACHE_DIR, 'media', hashed, type);
   await mkdir(tempDir, { recursive: true });
 
   return tempDir;
