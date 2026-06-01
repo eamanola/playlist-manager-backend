edd42ae08f91dca168cb6bf3cd15fda3c96f8a9e
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-01 13:44:13 +0300

    simplify trancode output handling

--- src/stream/transcode-stream.js
+++ src/stream/transcode-stream.js
@@ -1,12 +1,12 @@
-const { exec } = require('node:child_process');
-const { rm, cp } = require('node:fs/promises');
+const { exec, spawn } = require('node:child_process');
+const { rm, rename } = require('node:fs/promises');
+const { createWriteStream } = require('node:fs');
 
 const { utils } = require('automata-utils');
 const kill = require('tree-kill');
 
 const { cachePath, tmpPath } = require('./output-path');
 const { transcode } = require('./format');
-const streamOutput = require('./stream-output');
 
 const { logger } = utils;
 
@@ -21,16 +21,6 @@ const logProgress = ({ pid }) => (stderr) => {
     if (Number(speed) < 1) {
       logger.warn(pid, 'slow transcode, consider lowering quality');
     }
-  }
-};
-
-const onConnectionClosed = (proc) => () => {
-  logger.info(proc.pid, 'connection closed');
-
-  if (proc.exitCode === null) {
-    logger.info(proc.pid, 'killing');
-
-    kill(proc.pid, 'SIGKILL');
   }
 };
 
@@ -53,11 +43,15 @@ const postProcess = (mime, tmpFile, cacheFile) => {
     ];
     logger.info('-', [cmd1, ...args1].join(' '));
 
-    exec([cmd1, ...args1].join(' '), (moveErr) => logger.info(moveErr));
-  } else {
-    logger.info('copy to cache');
+    exec([cmd1, ...args1].join(' '), (moveErr) => {
+      logger.info(moveErr);
 
-    cp(tmpFile, cacheFile);
+      logger.info('remove tmp');
+      rm(tmpFile);
+    });
+  } else {
+    logger.info('move tmp to cache');
+    rename(tmpFile, cacheFile);
   }
 };
 
@@ -79,11 +73,15 @@ const transcodeStream = (type) => async (req, res, next) => {
     '-stats',
     '-i',
     `"${path}"`,
-    '-map_chapters -1 -map_metadata -1',
+    '-map_chapters',
+    '-1',
+    '-map_metadata',
+    '-1',
     '-map',
     `0:${streamIndex}`,
     ...codecOptions.split(' '),
-    `"${output}"`,
+    // `"${output}"`,
+    'pipe:1',
   ];
   logger.info('-', [cmd, ...args].join(' '));
 
@@ -95,23 +93,47 @@ const transcodeStream = (type) => async (req, res, next) => {
     );
   };
 
-  const onFail = (err) => {
-    logger.info(/* proc.pid, */ 'removing output');
-    rm(output);
-
-    next(err);
-  };
-
   res.setHeader('content-type', mime);
 
-  const proc = streamOutput(
-    [cmd, ...args].join(' '),
-    output,
-    res,
-    { onFail, onSuccess },
-  );
+  const cache = createWriteStream(output);
+  const proc = spawn([cmd, ...args].join(' '), null, { shell: true });
+  proc.stdout.pipe(res);
+  proc.stdout.pipe(cache);
 
-  req.on('close', onConnectionClosed(proc));
+  proc.on('close', (code, signal) => {
+    const success = code === 0 && signal === null;
+    logger.info(proc.pid, 'close:', 'success:', success, 'code:', code, 'signal:', signal);
+
+    if (!success) {
+      logger.info(proc.pid, 'removing cache');
+      rm(output);
+
+      next({ message: 'transcode failed' });
+    } else {
+      onSuccess();
+    }
+  });
+
+  cache.on('close', () => {
+    console.log(proc.pid, 'cache closed');
+  });
+
+  proc.on('exit', (code, signal) => {
+    console.log(proc.pid, 'exit', code, signal);
+  });
+
+  req.on('close', () => {
+    logger.info(proc.pid, 'connection closed');
+
+    const HEAD_START = 0;
+    setTimeout(() => {
+      if (proc.exitCode === null) {
+        logger.info(proc.pid, 'killing');
+
+        kill(proc.pid, 'SIGKILL');
+      }
+    }, HEAD_START);
+  });
 
   proc.stderr.on('data', logProgress(proc));
 };
