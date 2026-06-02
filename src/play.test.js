5c5156a5212d30fb0fc8e5cf72a3b7b766c0eee0
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-02 14:53:51 +0300

    fix play test

--- src/play.test.js
+++ src/play.test.js
@@ -1,42 +1,47 @@
-// const express = require('express');
-// const supertest = require('supertest');
-// const { errors } = require('automata-utils');
+const express = require('express');
+const supertest = require('supertest');
+const { errors } = require('automata-utils');
 
-// const canAccess = require('./utils/can-access');
-// const play = require('./cli/play');
-// const { MEDIA_LIBS } = require('./config');
-// const { router } = require('./play');
+const canAccess = require('./utils/can-access');
+const play = require('./cli/play');
+const { router } = require('./play');
+const { getVideos: initTempCache } = require('./videos');
+const cache = require('./temp-cache');
 
-// const app = express();
-// app.use(express.json());
-// app.use('/play', router);
-// const api = supertest(app);
+const app = express();
+app.use(express.json());
+app.use('/play', router);
+const api = supertest(app);
 
-// jest.mock('./cli/play');
-// jest.mock('./temp-cache', () => ({ getId: (path) => path }));
+jest.mock('./cli/play');
 
-// describe('play router', () => {
-//   afterEach(() => play.mockClear());
+describe('play router', () => {
+  afterEach(() => play.mockClear());
+  beforeAll(() => initTempCache());
 
-//   it('should call play with path', async () => {
-//     const path = `${MEDIA_LIBS}/foo`;
+  it('should call play with path', async () => {
+    const { id } = cache.all()[0];
+    const path = cache.getPath(id);
 
-//     await api.put('/play').send({ id: path });
+    await api.put('/play').send({ id });
 
-//     expect(play).toHaveBeenCalledTimes(1);
-//     expect(play).toHaveBeenCalledWith(path);
-//   });
+    expect(play).toHaveBeenCalledTimes(1);
+    expect(play).toHaveBeenCalledWith(path);
+  });
 
-//   it('should throw access denied', async () => {
-//     const path = '/foo';
-//     expect(canAccess(path)).toBe(false);
-//     const { status } = await api.put('/play').send({ id: path });
+  it('should throw access denied', async () => {
+    const path = '/foo';
+    cache.set([{ name: '', path }]);
+    const { id } = cache.all()[0];
 
-//     const { accessDenied } = errors;
-//     expect(status).toBe(accessDenied.status);
-//     expect(play).not.toHaveBeenCalled();
-//   });
-// });
+    expect(canAccess(path)).toBe(false);
+    const { status } = await api.put('/play').send({ id });
+
+    const { accessDenied } = errors;
+    expect(status).toBe(accessDenied.status);
+    expect(play).not.toHaveBeenCalled();
+  });
+});
 
 it('contains one test', () => {
   expect(1).toBe(1);
