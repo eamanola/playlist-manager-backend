0ec913d06343f66e63baef12e8eb3eb475febd3c
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-12 16:07:32 +0300

    connect anilist(DISABLED)

--- src/metas.js
+++ src/metas.js
@@ -1,12 +1,42 @@
-// const express = require('express');
-const router = () => (req, res/* , next */) => {
+const { anilist } = require('anilist');
+const { logger } = require('automata-utils');
+
+const DISABLE = true;
+
+const router = () => async (req, res/* , next */) => {
   const { query } = req;
   const { q } = query;
 
   const searchQueries = (Array.isArray(q) ? q : [q])
     .map((aQuery) => decodeURIComponent(aQuery));
 
-  res.status(200).json(searchQueries.map((aQuery) => ({ q: aQuery })));
+  // const foo = ['attack on titan', 'test1s2failNotfound'];
+
+  const responses = [];
+
+  for (let i = 0; i < searchQueries.length; i += 1) {
+    const aQuery = searchQueries[i];
+    let response = null;
+    try {
+      response = DISABLE
+        ? null
+        // eslint-disable-next-line no-await-in-loop
+        : await anilist.query.media(aQuery)
+          .withId()
+          .withTitles()
+          .withCoverImage()
+          .fetch();
+    } catch {
+      logger.info(aQuery, 'not found');
+    }
+
+    responses.push({
+      meta: response,
+      q: aQuery,
+    });
+  }
+
+  res.status(200).json(responses);
 };
 
 module.exports = { router };
